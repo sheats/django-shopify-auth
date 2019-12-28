cdecd9b81f519edc17b32f6e72923b89175b322d
Peter Sheats <sheats@gmail.com>
2019-12-27 19:59:55 -0500

    i think this will allow django admin to work and multiple backends

--- shopify_auth/models.py
+++ shopify_auth/models.py
@@ -1,18 +1,17 @@
 import shopify
 
 from django.conf import settings
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 
 class ShopUserManager(BaseUserManager):
-
     def create_user(self, myshopify_domain, password=None):
         """
         Creates and saves a ShopUser with the given domain and password.
         """
         if not myshopify_domain:
-            raise ValueError('ShopUsers must have a myshopify domain')
+            raise ValueError("ShopUsers must have a myshopify domain")
 
         user = self.model(myshopify_domain=myshopify_domain)
 
@@ -29,17 +28,23 @@ class ShopUserManager(BaseUserManager):
         return self.create_user(myshopify_domain, password)
 
 
-class AbstractShopUser(AbstractBaseUser):
+class AbstractShopUser(AbstractUser):
     myshopify_domain = models.CharField(max_length=255, unique=True, editable=False)
-    token = models.CharField(max_length=32, editable=False, default='00000000000000000000000000000000')
+    token = models.CharField(
+        max_length=32, editable=False, default="00000000000000000000000000000000"
+    )
 
     objects = ShopUserManager()
 
-    USERNAME_FIELD = 'myshopify_domain'
+    USERNAME_FIELD = "myshopify_domain"
 
     @property
     def session(self):
-        return shopify.Session.temp(self.myshopify_domain, getattr(settings, 'SHOPIFY_APP_API_VERSION', 'unstable'), self.token)
+        return shopify.Session.temp(
+            self.myshopify_domain,
+            getattr(settings, "SHOPIFY_APP_API_VERSION", "unstable"),
+            self.token,
+        )
 
     def get_full_name(self):
         return self.myshopify_domain
